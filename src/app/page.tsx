313455878a5c911b97f2d0f6214277fe49a41c0f
dana salem <drdanasalem@gmail.com>
2025-08-09 22:28:19 +0000

    هاد لينك الصورة الثالثه الي بدنا نضيفها بشريط الصورhttps://i.ibb.co/pvCj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -213,7 +213,7 @@ export default function Home() {
                         <CarouselItem className="md:basis-1/2 lg:basis-1/3 p-4">
                             <div className="group relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
                                 <Image
-                                    src="https://placehold.co/400x300.png"
+                                    src="https://i.ibb.co/pvCjpbbV/Untitled-design-7.png"
                                     alt="ذكرى مع الطلاب 3"
                                     width={400}
                                     height={300}
