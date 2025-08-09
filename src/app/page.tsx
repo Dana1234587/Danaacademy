df81ca3a320a86393831ed36cab47c02828ffe24
dana salem <drdanasalem@gmail.com>
2025-08-09 14:43:56 +0000

    بس بدي الموقع بس انزل يتحركو الايقونات زي floating يعني يكونو خلفيه للمو

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,12 +21,12 @@ export default function Home() {
         {/* Hero Section */}
         <div className="relative h-[600px] flex items-center justify-center text-center bg-white overflow-hidden">
             <div aria-hidden="true" className="absolute inset-0 z-0">
-                <BackgroundIcon icon={Atom} className="w-32 h-32 top-1/4 left-1/4 animate-pulse" />
-                <BackgroundIcon icon={Rocket} className="w-24 h-24 top-1/2 right-1/4 -rotate-45 animate-pulse delay-500" />
-                <BackgroundIcon icon={BrainCircuit} className="w-48 h-48 bottom-10 left-20 opacity-50" />
-                <BackgroundIcon icon={Lightbulb} className="w-20 h-20 top-20 right-48 animate-pulse delay-1000" />
-                <BackgroundIcon icon={Beaker} className="w-28 h-28 bottom-24 right-32 rotate-12" />
-                <BackgroundIcon icon={Orbit} className="w-36 h-36 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-30" />
+                <BackgroundIcon icon={Atom} className="w-32 h-32 top-1/4 left-1/4 animate-float" />
+                <BackgroundIcon icon={Rocket} className="w-24 h-24 top-1/2 right-1/4 -rotate-45 animate-float animation-delay-500" />
+                <BackgroundIcon icon={BrainCircuit} className="w-48 h-48 bottom-10 left-20 opacity-50 animate-float animation-delay-1000" />
+                <BackgroundIcon icon={Lightbulb} className="w-20 h-20 top-20 right-48 animate-float animation-delay-1500" />
+                <BackgroundIcon icon={Beaker} className="w-28 h-28 bottom-24 right-32 rotate-12 animate-float animation-delay-2000" />
+                <BackgroundIcon icon={Orbit} className="w-36 h-36 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-30 animate-float animation-delay-2500" />
             </div>
 
             <div className="relative z-10 p-4 space-y-6">
