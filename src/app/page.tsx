f9eb46d4c2ceccf3c3afe9292bbf6c1c000538d8
dana salem <drdanasalem@gmail.com>
2025-08-09 22:36:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -213,7 +213,7 @@ export default function Home() {
                         <CarouselItem className="basis-full md:basis-1/3 p-4">
                             <div className="group relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
                                 <Image
-                                    src="https://i.ibb.co/pvCjpbbV/Untitled-design-7.png"
+                                    src="https://i.ibb.co/4g9Ws86y/Untitled-design-9.png"
                                     alt="ذكرى مع الطلاب 3"
                                     width={400}
                                     height={300}
@@ -225,7 +225,7 @@ export default function Home() {
                         <CarouselItem className="basis-full md:basis-1/3 p-4">
                             <div className="group relative overflow-hidden rounded-lg shadow-lg transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-2xl">
                                 <Image
-                                    src="https://i.ibb.co/4g9Ws86y/Untitled-design-9.png"
+                                    src="https://placehold.co/400x300.png"
                                     alt="ذكرى مع الطلاب 4"
                                     width={400}
                                     height={300}
