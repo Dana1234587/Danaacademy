c01b6b75b6483f0cb7b792d518c3d21142e70432
dana salem <drdanasalem@gmail.com>
2025-08-14 17:37:05 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,20 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// =================================================================
-// TODO: استبدلي هذا الكائن بالإعدادات من مشروعك على Firebase
-// 1. اذهبي إلى console.firebase.google.com
-// 2. أنشئي مشروعًا جديدًا أو اختاري مشروعًا موجودًا.
-// 3. أضيفي تطبيق ويب (Web App).
-// 4. انسخي كائن firebaseConfig من هناك والصقيه هنا.
-// =================================================================
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY", //  <-- ضعي القيمة هنا
-  authDomain: "YOUR_AUTH_DOMAIN", //  <-- ضعي القيمة هنا
-  projectId: "YOUR_PROJECT_ID", //  <-- ضعي القيمة هنا
-  storageBucket: "YOUR_STORAGE_BUCKET", //  <-- ضعي القيمة هنا
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID", //  <-- ضعي القيمة هنا
-  appId: "YOUR_APP_ID" //  <-- ضعي القيمة هنا
+  apiKey: "AIzaSyBN3aaAQerX3GU2BPNwjiHcgdJ7ne-3dPw",
+  authDomain: "danaacademy-d6b29.firebaseapp.com",
+  projectId: "danaacademy-d6b29",
+  storageBucket: "danaacademy-d6b29.firebasestorage.app",
+  messagingSenderId: "1039366223124",
+  appId: "1:1039366223124:web:2ee9ff51f64bf9042c3e01",
+  measurementId: "G-K77THJQ4ZL"
 };
 
 // Initialize Firebase
