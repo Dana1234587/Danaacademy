4df01ea12066ab005f9dd99d3a97be908795762b
dana salem <drdanasalem@gmail.com>
2025-08-14 17:14:13 +0000

    نفذ

---
+++ src/content/physics/supplementary-2007/first-semester/3-electric-current/1-resistance-and-emf/1-current-concept/quiz.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const QuizPage = () => {
+  return (
+    <div>
+      <h1>الأسئلة والاختبارات</h1>
+      <p>هذا الملف سيحتوي على الأسئلة والاختبارات المتعلقة بالجزء.</p>
+    </div>
+  );
+};
+
+export default QuizPage;
---
+++ src/content/physics/supplementary-2007/first-semester/4-magnetic-field/1-field-from-current/3-field-from-long-wire/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContentPage = () => {
+  return (
+    <div>
+      <h1>المحتوى الرئيسي والشرح</h1>
+      <p>هذا الملف سيحتوي على الشرح المفصل للجزء.</p>
+    </div>
+  );
+};
+
+export default ContentPage;
