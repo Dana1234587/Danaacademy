a6619e05df0d89ac46a53ca68fc72a611412ec25
dana salem <drdanasalem@gmail.com>
2025-08-29 17:14:16 +0000

    صديقي طريقة العرض مختلفه عن طريقة عرض الفصل الاول بالفصل الاول الفيديو

--- src/app/courses/physics-supplementary-2007/second-semester/5-electromagnetic-induction-and-semiconductors/1-magnetic-flux-and-electromagnetic-induction/1-magnetic-flux-concept/page.tsx
+++ src/app/courses/physics-supplementary-2007/second-semester/5-electromagnetic-induction-and-semiconductors/1-magnetic-flux-and-electromagnetic-induction/1-magnetic-flux-concept/page.tsx
@@ -31,7 +31,7 @@ export default function MagneticFluxConceptPage() {
             
             <div className="relative w-full rounded-lg overflow-hidden shadow-lg" style={{ paddingBottom: '56.25%' }}>
                 <iframe
-                    src="https://iframe.mediadelivery.net/play/480623/ea8098bd-172c-4226-a1b3-4c33c2e226e7"
+                    src="https://iframe.mediadelivery.net/embed/480623/ea8098bd-172c-4226-a1b3-4c33c2e226e7?autoplay=false&loop=false&muted=false&preload=true"
                     className="absolute top-0 left-0 w-full h-full border-0"
                     loading="lazy"
                     allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
