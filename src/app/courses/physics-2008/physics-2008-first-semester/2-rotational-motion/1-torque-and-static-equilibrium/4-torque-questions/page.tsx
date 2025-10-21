aaefbb5cb8236c3a32ed3cd57a151333be016683
dana salem <drdanasalem@gmail.com>
2025-10-21 13:42:52 +0000

    هاد لينك ورقة عمل الحصة الرابعه عزم الازدواج والخامسه مركز الكتلة ضيفهم

--- src/app/courses/physics-2008/physics-2008-first-semester/2-rotational-motion/1-torque-and-static-equilibrium/4-torque-questions/page.tsx
+++ src/app/courses/physics-2008/physics-2008-first-semester/2-rotational-motion/1-torque-and-static-equilibrium/4-torque-questions/page.tsx
@@ -52,8 +52,10 @@ export default function CoupleTorquePage() {
                 <CardTitle>موارد إضافية</CardTitle>
               </CardHeader>
               <CardContent className="space-y-3">
-                <Button variant="ghost" className="w-full justify-start">
-                  <FileText className="me-3"/> ورقة عمل الدرس (PDF)
+                <Button variant="ghost" className="w-full justify-start" asChild>
+                  <Link href="https://drive.google.com/file/d/1QO4r7ZirO5H_BMKFlF1fjRCvuZvcS3QZ/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <FileText className="me-3"/> ورقة عمل الدرس (PDF)
+                  </Link>
                 </Button>
                 <Dialog>
                     <DialogTrigger asChild>
